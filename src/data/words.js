ca4cd58475a70de4362fe2fad878361283471030
Monique Caldeira <86532831+MoniqueCaldeira@users.noreply.github.com>
2024-09-12 20:52:55 +0000

    adicição de novas palavras

--- src/data/words.js
+++ src/data/words.js
@@ -10,7 +10,13 @@ export const wordsList = {
  };*/
 
  export const wordsList = {
-  "Elemento HTML": ["iframe", "header", "body","section", "div", "title", "img"],
-  "Propriedade CSS": ["color", "background", "padding", "display", "gap", "font-size", "margin", "font-family", "opacity"],
-  "Propriedade HTML": ["href", "class", "src"],
+  "É um Elemento HTML": ["iframe", "header", "body", "section", "div", "title", "img"],
+  "É uma Propriedade CSS": ["color", "background", "padding", "display", "font-size", "margin", "font-family"],
+  "É uma Propriedade HTML": ["href", "class"],
+  "É uma Linguagem de Marcação" : ["html"],
+  "É uma Linguagem de Estilização" : ["css"],
+  "É uma Linguagem de Programação" : ["javascript", "scratch"],
+  "É um Ambiente de Desenvolvimento" : ["vscode"],
+  "É uma Ferramenta de Armazenamento" : ["drive"],
+  "É um Repositório" : ["github"],
 };
