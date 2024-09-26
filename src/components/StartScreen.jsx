31be9fac1e2a888076519a33cbfcc56324d5cf60
Monique Caldeira <86532831+MoniqueCaldeira@users.noreply.github.com>
2024-09-26 21:11:18 +0000

    alteração das palavras e dicas

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -4,8 +4,7 @@ import logo from "../assets/Start_logo-01.png"
 const StartScreen = ({startGame}) => {
   return (
     <div className='start'>
-      <h1>Roda Roda</h1>
-      <img className="logo" src={logo}/>
+      <h1>Roda Roda Start</h1>
       <p>Clique no botão abaixo para começar a jogar!</p>
       <button onClick={startGame}>Começar o Jogo</button>
     </div>
--- src/data/words.js
+++ src/data/words.js
@@ -1,14 +1,14 @@
-/*
-export const wordsList = {
-   escola: ["Caderno", "Lapis", "Professor", "Aluno", "Quadro", "Livro", "Lousa", "Carteira", "Mochila", "Biblioteca"],
-   fruta: ["Banana", "Laranja", "Abacaxi", "Morango", "Uva", "Melancia", "Kiwi"],
-   educação: ["Ensino", "Aprendizagem", "Didatica", "Pedagogia", "Curriculo", "Docente", "Discente", "Metodologia"],
-   computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador", "Memoria", "Fonte"],
-   programação: ["Linguagem", "JavaScript", "Scratch", "Variavel", "Funçao", "Compilador", "Bug", "Algoritmo", "Biblioteca", "Lista", "Loop", "Condicional", "Recursao"],
-   alimento: ["Arroz", "Feijao", "Leite", "Queijo", "Manteiga"],
-   animais: ["Cachorro", "Gato", "Elefante", "Tigre", "Girafa", "Pato", "Coelho", "Cavalo"],
- };*/
 
+export const wordsList = {
+   "Tem na escola": ["Caderno", "Lapis", "Professor", "Aluno", "Quadro", "Livro", "Lousa", "Carteira", "Mochila", "Biblioteca"],
+   "É uma fruta": ["Banana", "Laranja", "Abacaxi", "Morango", "Uva", "Melancia", "Kiwi"],
+   "Educação": ["Ensino", "Aprendizagem", "Didatica", "Pedagogia", "Curriculo", "Docente", "Discente", "Metodologia"],
+   "É um componente do computador": ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador", "Memoria", "Fonte"],
+   "Está relacionado com programação": ["Linguagem", "JavaScript", "Scratch", "Variavel", "Funçao", "Compilador", "Bug", "Algoritmo", "Biblioteca", "Lista", "Loop", "Condicional", "Recursao"],
+   "É um tipo de alimento": ["Arroz", "Feijao", "Leite", "Queijo", "Manteiga"],
+   "É um animal": ["Cachorro", "Gato", "Elefante", "Tigre", "Girafa", "Pato", "Coelho", "Cavalo"],
+ };
+/*
  export const wordsList = {
   "É um Elemento HTML": ["iframe", "header", "body", "section", "div", "title", "img"],
   "É uma Propriedade CSS": ["color", "background", "padding", "display", "font-size", "margin", "font-family"],
@@ -18,3 +18,4 @@ export const wordsList = {
   "É uma Linguagem de Programação" : ["javascript", "scratch"],
   "É um Ambiente de Desenvolvimento" : ["vscode"],
 };
+*/
